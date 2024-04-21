4ee8b22e75824a54a13094004b4e5311beea5595
Alibenaisha <alibenaisha3@gmail.com>
2024-04-21 04:42:32 +0200

    end new user
    edit

--- resources/views/certs/add_cert.blade.php
+++ resources/views/certs/add_cert.blade.php
@@ -7,32 +7,32 @@
     active
 @endsection
 @section('content')
-    <div class="wrapper-two">
+    <div class="wrapper-two" dir="rtl">
         <h3 class="title-main">إضافة مؤهل عملي او دورة</h3>
-        <form class="row g-4 needs-validation certs" novalidate>
-            <div class="row g-2">
-                <div class="col-md-3">
+        <form class="row g-4 needs-validation certs mt-4" novalidate>
+            <div class="row g-2"  style="width: 500px;     margin: 0 auto;">
+                <div class="col-md-6">
                     <label for="validationCustom01" class="form-label">الرقم</label>
                     <input type="number" name="id_cert_0" class="form-control" id="validationCustom01" required>
                     <div class="valid-feedback">
                         Looks good!
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-6">
                     <label for="validationCustom02" class="form-label">رقم الموظف</label>
                     <input type="number" name="id_emp_0" class="form-control" id="validationCustom02" required>
                     <div class="valid-feedback">
                         Looks good!
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-12">
                     <label for="validationCustom03" class="form-label">الإسم</label>
                     <input type="text" name="name_emp_0" class="form-control" id="validationCustom03"  required>
                     <div class="valid-feedback">
                         Looks good!
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-6">
                     <label for="validationCustom04" class="form-label">العنوان</label>
                     <input type="text" name="title_cert_0" class="form-control" id="validationCustom04" required>
                     <div class="invalid-feedback">
@@ -55,11 +55,12 @@
                     <input type="file" name="file_0" class="form-control" aria-label="file example" required>
                     <div class="invalid-feedback">Example invalid form file feedback</div>
                 </div>
+                <div class="row addemp  " style="width: 500px;     margin: 40px 0 0 13px !important;">
+                       <!-- <a href="" class="search">بحث</a> -->
+                        <a class="add p-2 text-center" href="users/create">إضافة </a>
+                    </div>
             </div>
 
-            <div class="col-12">
-                <button class="btn btn-primary" type="submit">إضافة</button>
-            </div>
         </form>
     </div>
 @endsection
--- resources/views/users/add_user.blade.php
+++ resources/views/users/add_user.blade.php
@@ -7,38 +7,40 @@
     active
 @endsection
 @section('content')
-    <div class="wrapper-two">
+    <div class="wrapper-two" dir="rtl">
         <h3 class="title-main">إضافة مستخدم</h3>
-        <form class="row g-4 needs-validation" novalidate>
-            <div class="col-md-2">
+        <form class="row g-4 needs-validation mt-5" novalidate>
+        <div class="row" style="width: 500px;     margin: 0 auto;">
+
+            <div class="col-md-12">
                 <label for="validationCustom02" class="form-label">رقم المستخدم</label>
                 <input type="text" class="form-control" id="validationCustom02" required>
                 <div class="valid-feedback">
                     Looks good!
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-12">
                 <label for="validationCustom02" class="form-label">رقم الموظف</label>
                 <input type="text" class="form-control" id="validationCustom02" required>
                 <div class="valid-feedback">
                     Looks good!
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-12">
                 <label for="validationCustom01" class="form-label">الإسم</label>
                 <input type="text" class="form-control" id="validationCustom01"  required>
                 <div class="valid-feedback">
                     Looks good!
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-12">
                 <label for="validationCustom03" class="form-label">اسم المستخدم</label>
                 <input type="text" class="form-control" id="validationCustom03" required>
                 <div class="invalid-feedback">
                     Please provide a valid city.
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-12">
                 <label for="validationCustom03" class="form-label">كلمة السر</label>
                 <input type="text" class="form-control" id="validationCustom03" required>
                 <div class="invalid-feedback">
@@ -46,8 +48,10 @@
                 </div>
             </div>
 
-            <div class="col-12">
-                <button class="btn btn-primary" type="submit">إضافة</button>
+            <div class="row addemp  " style="width: 500px;     margin: 40px 0 0 13px !important;">
+                       <!-- <a href="" class="search">بحث</a> -->
+                        <a class="add p-2 text-center" href="users/create">إضافة مستخدم</a>
+                    </div>
             </div>
         </form>
     </div>
